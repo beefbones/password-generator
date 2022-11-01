308b6507d7167a150b9a9362dc9b972d9d964387
Nolan Ross <nolanross@Nolans-MacBook-Pro.local>
2022-10-31 22:05:22 -0400

    finished pseudocode, added README

--- Develop/script.js
+++ Develop/script.js
@@ -6,18 +6,19 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "+", "-", "=", "_"];
+// Make an empty array that shows what options were chosen
 var options = [];
 var password = [];
-
+// Create a function to generate a random integer
 function randomInt(min,max) {
   if (!max) {
     max = min
     min = 0
   }
-  let rand = Math.random()
+  var rand = Math.random()
   return Math.floor(min*(1 - rand) + rand*max)
 };
-
+// Create a function to return list length
 function getRandomItem(list) {
   return list[randomInt(list.length)]
 };
@@ -26,20 +27,20 @@ function getRandomItem(list) {
 function generatePassword() {
   var userInput = prompt("How long would you like your password to be? (Enter a number)");
   var passwordLength = parseInt(userInput);
-
+// If user selects cancel to first prompt, it will exit the function
   if (userInput === null) {
     return
   }
 
   // Make an if statement if they enter something that is NaN (Not a number)
   if (isNaN(passwordLength)) {
-    alert("That is not a number!")
+    alert("That is not a number!");
     return
   }
 
   // Make an if statement that says password length must be between 8 and 128 characters
   if(passwordLength < 8 || passwordLength > 128) {
-    alert("Password length must be between 8 and 128 characters.")
+    alert("Password length must be between 8 and 128 characters.");
     return
   }
 
@@ -53,41 +54,34 @@ function generatePassword() {
   
   // Make if statements for all options
   if (addLowercase) {
-    options.push(lowercase)
-  };
+    options.push(...lowercase);
+    
+  }
 
   if (addUppercase) {
-    options.push(uppercase)
-  };
+    options.push(...uppercase);
+  }
 
   if (addNumbers) {
-    options.push(numbers)
-  };
+    options.push(...numbers);
+  }
 
   if (addCharacters) {
-    options.push(characters)
-  };
-
+    options.push(...characters);
+  }
+// If none of the options are chosen, push lowercase only
   if (options.length === 0) {
     options.push(lowercase);
   }
 
   var generatedPassword = ""
-
+// Write a for loop that chooses random characters that iterates over chosen arrays
   for (var i = 0; i < passwordLength; i++) {
-    var randomList = getRandomItem(options)
-    var randomChar = getRandomItem(randomList)
+    var randomChar = getRandomItem(options);
     generatedPassword += randomChar
   }
-
+// Push the generated password to the display
   return generatedPassword
-  // Make an array that shows what options were chosen
-
-  // Write a for loop that chooses random characters that iterates over chosen arrays
-
-  // Write an if statement if none of the options are chosen
-
-  // Push the generated password to the display
 };
 
 // Write password to the #password input
